f07450f6ff28496aa36245b2cb352fbcdaad2588
Kris Parker <kaypy>
2009-10-30 17:44:30 +0000

    Bug in x,y,z display bounds

--- WorldSegment.cpp
+++ WorldSegment.cpp
@@ -79,7 +79,7 @@ void WorldSegment::drawAllBlocks(BITMAP* target){
     // x,y,z print pricess
     uint32_t vsxmax = viewedSegment->x + viewedSegment->sizex;
     uint32_t vsymax = viewedSegment->y + viewedSegment->sizey;
-    uint32_t vszmax = viewedSegment->y + viewedSegment->sizez;
+    uint32_t vszmax = viewedSegment->z + viewedSegment->sizez;
     for(uint32_t vsx=viewedSegment->x; vsx < vsxmax; vsx++){
         for(uint32_t vsy=viewedSegment->y; vsy < vsymax; vsy++){
             for(uint32_t vsz=viewedSegment->z; vsz < vszmax; vsz++){
