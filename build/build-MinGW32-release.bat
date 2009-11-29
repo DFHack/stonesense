3cffec41260c8655196ac50b9a5ce2ff45142d94
Kris Parker <kaypy>
2009-11-29 16:41:45 +0000

    added suess' icon

--- build/build-MinGW32-release.bat
+++ build/build-MinGW32-release.bat
@@ -1,5 +1,8 @@
 mkdir build-real
 cd build-real
+set path=c:\Program Files\CMake 2.6\bin;c:\MinGW\bin;%path%
 cmake ..\.. -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE:string=Release
+rem CMake should do this, but wont cooperate
+windres -I..\.. -i ..\..\res.rc -o res_rc.obj 
 mingw32-make
 pause
