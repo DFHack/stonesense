d85bfdaeceb8e5777d8c39dddd5b27c437c122f5
Japa <japa.mala.illo@gmail.com>
2011-03-28 11:19:51 +0000

    upgraded to MSVC 2010, changed some wood colors to match the tree colors.

--- allegro/allegro5/internal/aintern_image_cfg.h
+++ allegro/allegro5/internal/aintern_image_cfg.h
@@ -1,9 +1,9 @@
 /* which libraries are present and needed? */
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS */
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS_LOWERCASE_H */
-/* #undef ALLEGRO_CFG_IIO_HAVE_PNG */
-/* #undef ALLEGRO_CFG_IIO_HAVE_JPG */
+#define ALLEGRO_CFG_IIO_HAVE_PNG
+#define ALLEGRO_CFG_IIO_HAVE_JPG
 
 /* which formats are supported and wanted? */
-/* #undef ALLEGRO_CFG_IIO_SUPPORT_PNG */
-/* #undef ALLEGRO_CFG_IIO_SUPPORT_JPG */
+#define ALLEGRO_CFG_IIO_SUPPORT_PNG
+#define ALLEGRO_CFG_IIO_SUPPORT_JPG
