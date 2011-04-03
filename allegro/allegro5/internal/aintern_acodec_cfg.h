244db2cf0b316c58f52fe530a397455b9088c833
Japa <japa.mala.illo@gmail.com>
2011-04-03 18:30:48 +0000

    Reverted the upgrade to MSVC2010. now we are back to 2005.

--- allegro/allegro5/internal/aintern_acodec_cfg.h
+++ allegro/allegro5/internal/aintern_acodec_cfg.h
@@ -1,8 +1,8 @@
-#define ALLEGRO_CFG_ACODEC_FLAC
+/* #undef ALLEGRO_CFG_ACODEC_FLAC */
 /* #undef ALLEGRO_CFG_ACODEC_MODAUDIO */
-#define ALLEGRO_CFG_ACODEC_VORBIS
+/* #undef ALLEGRO_CFG_ACODEC_VORBIS */
 
 /* Define if the library should be loaded dynamically. */
-#define ALLEGRO_CFG_ACODEC_FLAC_DLL "FLAC.dll"
+/* #undef ALLEGRO_CFG_ACODEC_FLAC_DLL */
 /* #undef ALLEGRO_CFG_ACODEC_DUMB_DLL */
-#define ALLEGRO_CFG_ACODEC_VORBISFILE_DLL "vorbisfile.dll"
+/* #undef ALLEGRO_CFG_ACODEC_VORBISFILE_DLL */
--- allegro/allegro5/internal/aintern_image_cfg.h
+++ allegro/allegro5/internal/aintern_image_cfg.h
@@ -1,9 +1,9 @@
 /* which libraries are present and needed? */
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS */
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS_LOWERCASE_H */
-#define ALLEGRO_CFG_IIO_HAVE_PNG
-#define ALLEGRO_CFG_IIO_HAVE_JPG
+/* #undef ALLEGRO_CFG_IIO_HAVE_PNG */
+/* #undef ALLEGRO_CFG_IIO_HAVE_JPG */
 
 /* which formats are supported and wanted? */
-#define ALLEGRO_CFG_IIO_SUPPORT_PNG
-#define ALLEGRO_CFG_IIO_SUPPORT_JPG
+/* #undef ALLEGRO_CFG_IIO_SUPPORT_PNG */
+/* #undef ALLEGRO_CFG_IIO_SUPPORT_JPG */
