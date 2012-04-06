f1b38ee217bdc2e562e9f1d9df0b1b89015d2346
Caldfir <Aitken.Tim@gmail.com>
2012-04-05 19:40:37 -0700

    Added preliminary version of clothed female dwarves.

--- Creatures.cpp
+++ Creatures.cpp
@@ -278,6 +278,7 @@ void DrawCreature(int drawx, int drawy, t_unit* creature, Block * b){
 	unsigned int offsety = config.show_creature_names ? al_get_font_line_height(font) : 0;
 	
 	sprite->draw_world(creature->x,creature->y, creature->z, b);
+	
 	if(statusIcons.size())
 	{
 		for(int i = 0; i < statusIcons.size(); i++)
