DFHack/stonesense

commit 0fb9abc5f8a7511269ab072317bbd366bc2fa9ca

Remove unsafe usage of memcpy/memset and usage of malloc/free.

Tile.cpp
@@ -61,7 +61,89 @@ worn_item::worn_item()
     dyematt.type = -1;
 }
 
-Tile::Tile(WorldSegment* segment, df::tiletype type)
+Tile::Tile()
+{
+    // zero out everything
+    valid = false;
+    visible = false;
+
+    ownerSegment = nullptr;
+
+    x = 0;
+    y = 0;
+    z = 0;
+    tileType = tiletype::Void;
+    material.type = 0;
+    material.index = 0;
+    layerMaterial.type = 0;
+    layerMaterial.index = 0;
+    veinMaterial.type = 0;
+    veinMaterial.index = 0;
+    hasVein = false;
+
+    depthBorderNorth = false;
+    depthBorderWest = false;
+    depthBorderDown = false;
+
+    shadow = 0;
+
+    wallborders = 0;
+    floorborders = 0;
+    openborders = 0;
+    rampborders = 0;
+    upstairborders = 0;
+    downstairborders = 0;
+    lightborders = 0;
+
+    fog_of_war = false;
+
+    rampindex = 0;
+
+    deepwater = 0;
+
+    flow_direction = tile_liquid_flow_dir::none;
+
+    designation.whole = 0;
+    occ.whole = 0;
+
+    creature = nullptr;
+    tree.type = 0;
+    tree.index = 0;
+    tree_tile.whole = 0;
+
+    mudlevel = 0;
+    snowlevel = 0;
+    bloodlevel = 0;
+    ALLEGRO_COLOR bloodcolor;
+
+    grasslevel = 0;
+    grassmat = 0;
+
+    engraving_character = 0;
+    df::engraving_flags engraving_flags;
+    engraving_quality = 0;
+
+    consForm = 0;
+
+    obscuringCreature = false;
+    obscuringBuilding = false;
+
+    tileeffect.matt.type = 0;
+    tileeffect.matt.index = 0;
+    tileeffect.density = 0;
+    tileeffect.type = flow_type::Miasma;
+
+    memset(&Item, 0, sizeof(Item));
+
+    building.info = nullptr;
+    building.type = building_type::Chair;
+    building.sprites.clear();
+    building.parent = nullptr;
+    building.constructed_mats.clear();
+    building.special = 0;
+}
+
+Tile::Tile(WorldSegment* segment, df::tiletype type) : Tile()
 {
     //set all the nonzero values
     valid=true;
@@ -137,11 +219,13 @@ bool Tile::InvalidateAndDestroy(Tile* dst)
  */
 bool Tile::CleanCreateAndValidate(Tile* dst, WorldSegment* segment, df::tiletype type)
 {
-    bool ret = dst->valid;
-    memset(dst, 0, sizeof(Tile));
+    bool wasValid = dst->valid;
+    if (wasValid) {
+        dst->~Tile();
+    }
     new (dst) Tile(segment, type);
     dst->valid = true;
-    return ret;
+    return wasValid;
 }
 
 inline ALLEGRO_BITMAP* imageSheet(t_SpriteWithOffset sprite, ALLEGRO_BITMAP* defaultBmp)

Tile.h
@@ -17,13 +17,15 @@ class Tile
 private:
     bool valid;
 
-    //Functions start here.
+    // Functions start here.
 
-    //do not directly call constructor or deconstructor, use CleanCreateAndValidate or InvalidateAndDestroy
+    // use CleanCreateAndValidate or InvalidateAndDestroy
     Tile(WorldSegment* ownerSegment, df::tiletype type);
-    ~Tile(void);
 
 public:
+    Tile();
+    ~Tile();
+
     bool visible;
 
     WorldSegment* ownerSegment;

WorldSegment.h
@@ -46,8 +46,7 @@ public:
 
         uint32_t newNumTiles = inState.Size.x * inState.Size.y * inState.Size.z;
         uint32_t memoryNeeded = newNumTiles * sizeof(Tile);
-        tiles = (Tile*) malloc( memoryNeeded );
-        memset(tiles, 0, memoryNeeded);
+        tiles = new Tile[newNumTiles];
     }
 
     ~WorldSegment() {
@@ -57,7 +56,7 @@ public:
         }
         ClearBuildings();
         ClearUnits();
-        free(tiles);
+        delete[] tiles;
     }
 
     void Reset(GameState inState, bool hard=false) {
@@ -73,18 +72,8 @@ public:
         uint32_t memoryNeeded = newNumTiles * sizeof(Tile);
         //if this is a hard reset, or if the size doesn't match what is needed, get a new segment
         if(hard || newNumTiles != getNumTiles()) {
-            free(tiles);
-            tiles = (Tile*) malloc( memoryNeeded );
-
-            //on a hard reset, zero out the entire array
-            if(hard) {
-                memset(tiles, 0, memoryNeeded);
-            //otherwise, just invalidate the entire set of tiles
-            } else {
-                for(uint32_t i = 0; i < newNumTiles; i++) {
-                    tiles[i].Invalidate();
-                }
-            }
+            delete[] tiles;
+            tiles = new Tile[newNumTiles];
         }
 
         segState = inState;

CreatureConfiguration.cpp
@@ -12,23 +12,18 @@ using namespace DFHack;
 using namespace df::enums;
 
 
-CreatureConfiguration::CreatureConfiguration(int professionID, const char* professionStr, uint8_t sex, int incaste, enumCreatureSpecialCases special, c_sprite &sprite, int shadow)
+CreatureConfiguration::CreatureConfiguration(int professionID, const char* professionStr, uint8_t sex, int incaste, enumCreatureSpecialCases special, c_sprite &sprite, int shadow) :
+    professionstr(),
+    professionID(professionID),
+    sprite(sprite),
+    shadow(shadow),
+    special(special),
+    sex(sex),
+    caste(incaste)
 {
-    memset(this, 0, sizeof(CreatureConfiguration) );
-    this->sprite = sprite;
-    this->professionID = professionID;
-    this->sex = sex;
-    this->shadow = shadow;
-    this->special = special;
-    this->caste = incaste;
-
-    if(professionStr) {
-        int len = (int) strlen(professionStr);
-        if(len > CREATURESTRLENGTH) {
-            len = CREATURESTRLENGTH;
-        }
-        memcpy(this->professionstr, professionStr, len);
-        this->professionstr[CREATURESTRLENGTH-1]=0;
+    if (professionStr) {
+        strncpy(this->professionstr, professionStr, CREATURESTRLENGTH - 1);
+        this->professionstr[CREATURESTRLENGTH - 1] = 0;
     }
 }
 
@@ -94,7 +89,7 @@ bool addSingleCreatureConfig( TiXmlElement* elemCreature, vector<std::unique_ptr
     if (shadowStr != NULL && shadowStr[0] != 0) {
         baseShadow = atoi( shadowStr );
     }
-    if (baseShadow < 0 || baseShadow > MAX_SHADOW) {
+    if (baseShadow > MAX_SHADOW) {
         baseShadow = DEFAULT_SHADOW;
     }
     const char* filename = elemCreature->Attribute("file");

Creatures.cpp
@@ -532,15 +532,17 @@ void copyCreature(df::unit * source, SS_Unit & furball)
     // stress level
     furball.stress_level = source->status.current_soul ? source->status.current_soul->personality.stress_level : 0;
     // physical attributes
-    memcpy(&furball.strength, source->body.physical_attrs, sizeof(source->body.physical_attrs));
+    furball.strength = source->body.physical_attrs[physical_attribute_type::STRENGTH];
+    furball.agility = source->body.physical_attrs[physical_attribute_type::AGILITY];
+    furball.toughness = source->body.physical_attrs[physical_attribute_type::TOUGHNESS];
+    furball.endurance = source->body.physical_attrs[physical_attribute_type::ENDURANCE];
+    furball.recuperation = source->body.physical_attrs[physical_attribute_type::RECUPERATION];
+    furball.disease_resistance = source->body.physical_attrs[physical_attribute_type::DISEASE_RESISTANCE];
 
     // mood stuff
     furball.mood = source->mood;
     furball.mood_skill = source->job.mood_skill;
     Translation::readName(furball.artifact_name, &source->status.artifact_name);
-
-    // labors
-    memcpy(&furball.labors, &source->status.labors, sizeof(furball.labors));
 
     furball.birth_year = source->birth_year;
     furball.birth_time = source->birth_time;

GameBuildings.cpp
@@ -95,7 +95,7 @@ void MergeBuildingsToSegment(vector<Buildings::t_building>* buildings, WorldSegm
     for (uint32_t i = 0; i < numBuildings; i++) {
         auto building_ptr = dts::make_unique<Buildings::t_building>();
         auto copiedbuilding = building_ptr.get();
-        memcpy(copiedbuilding, &((*buildings)[i]), sizeof(Buildings::t_building));
+        *copiedbuilding = buildings->at(i);
         segment->PushBuilding(std::move(building_ptr));
 
         //int bheight = tempbuilding.y2 - tempbuilding.y1;

MapLoading.cpp
@@ -89,19 +89,18 @@ void readSpatterToTile(Tile * b, uint32_t lx, uint32_t ly,
             }
 
             //use the state information to determine if we should be incrementing the snow or the blood
-            int16_t state = splatter[i]->mat_state;
-            state = splatter[i]->mat_type == MUD ? INVALID_INDEX : state;
+            auto state = df::matter_state(splatter[i]->mat_state);
             state = splatter[i]->mat_type == VOMIT ? df::matter_state::Paste : state; //change vomit from dust to paste - gross
             switch(state) {
-            case INVALID_INDEX:
-                b->mudlevel = level;
-                break;
             case df::matter_state::Powder:
             case df::matter_state::Solid:
                 snow += level;
                 break;
             default:
-                blood += level;
+                if (splatter[i]->mat_type == MUD)
+                    b->mudlevel = level;
+                else
+                    blood += level;
             }
         }
         blood = blood<0 ? 0-blood : blood;

SpriteObjects.cpp
@@ -1321,6 +1321,7 @@ ALLEGRO_COLOR c_sprite::get_color(void* tile)
         } else {
             return al_map_rgb(255,255,255);
         }
+        break;
     case ShadeBlood:
         return b->bloodcolor;
     case ShadeEquip:
@@ -1345,6 +1346,7 @@ ALLEGRO_COLOR c_sprite::get_color(void* tile)
                 return al_map_rgb(255,255,255);
             }
         }
+        break;
     case ShadeJob:
         if(b->occ.bits.unit && b->creature) {
             return ssConfig.colors.getDfColor(Units::getProfessionColor(b->creature->origin), ssConfig.useDfColors);
@@ -1354,8 +1356,8 @@ ALLEGRO_COLOR c_sprite::get_color(void* tile)
     case ShadeItem:
         return lookupMaterialColor(b->Item.matt, b->Item.dyematt);
     default:
-        return al_map_rgb(255, 255, 255);
-    } ;
+        break;
+    }
     return al_map_rgb(255, 255, 255);
 }
 

commit f09e11050adcecfa419f291b135070259baf816a

Fix some Tile memory safety issues
0fb9abc5f8a7511269ab072317bbd366bc2fa9ca introduced some paths where Tile
instances could be double-freed: specifically, `WorldSegment::Reset()` would
call `Tile::InvalidateAndDestroy()` on individual tiles, followed by `delete[]`
on the array containing them, and both of these call the Tile destructor.

The `tiles` array was previously managed with `malloc()`/`free()`. Quietust
opted to switch to `new char[]` instead of `malloc()` to make it more obvious
that we are intentionally using placement-new instead of `new Tile[]`.

Co-authored-by: lethosor <lethosor@gmail.com>

Tile.cpp
@@ -61,89 +61,7 @@ worn_item::worn_item()
     dyematt.type = -1;
 }
 
-Tile::Tile()
-{
-    // zero out everything
-    valid = false;
-    visible = false;
-
-    ownerSegment = nullptr;
-
-    x = 0;
-    y = 0;
-    z = 0;
-    tileType = tiletype::Void;
-    material.type = 0;
-    material.index = 0;
-    layerMaterial.type = 0;
-    layerMaterial.index = 0;
-    veinMaterial.type = 0;
-    veinMaterial.index = 0;
-    hasVein = false;
-
-    depthBorderNorth = false;
-    depthBorderWest = false;
-    depthBorderDown = false;
-
-    shadow = 0;
-
-    wallborders = 0;
-    floorborders = 0;
-    openborders = 0;
-    rampborders = 0;
-    upstairborders = 0;
-    downstairborders = 0;
-    lightborders = 0;
-
-    fog_of_war = false;
-
-    rampindex = 0;
-
-    deepwater = 0;
-
-    flow_direction = tile_liquid_flow_dir::none;
-
-    designation.whole = 0;
-    occ.whole = 0;
-
-    creature = nullptr;
-    tree.type = 0;
-    tree.index = 0;
-    tree_tile.whole = 0;
-
-    mudlevel = 0;
-    snowlevel = 0;
-    bloodlevel = 0;
-    bloodcolor = al_map_rgba(0,0,0,0);
-
-    grasslevel = 0;
-    grassmat = 0;
-
-    engraving_character = 0;
-    engraving_flags.whole = 0;
-    engraving_quality = 0;
-
-    consForm = 0;
-
-    obscuringCreature = false;
-    obscuringBuilding = false;
-
-    tileeffect.matt.type = 0;
-    tileeffect.matt.index = 0;
-    tileeffect.density = 0;
-    tileeffect.type = flow_type::Miasma;
-
-    memset(&Item, 0, sizeof(Item));
-
-    building.info = nullptr;
-    building.type = building_type::Chair;
-    building.sprites.clear();
-    building.parent = nullptr;
-    building.constructed_mats.clear();
-    building.special = 0;
-}
-
-Tile::Tile(WorldSegment* segment, df::tiletype type) : Tile()
+Tile::Tile(WorldSegment* segment, df::tiletype type)
 {
     //set all the nonzero values
     valid=true;
@@ -187,18 +105,6 @@ bool Tile::IsValid()
 }
 
 /**
- * invalidates this Tile
- * returns old validity value
- */
-bool Tile::Invalidate(){
-    if(!valid) {
-        return false;
-    }
-    valid=false;
-    return true;
-}
-
-/**
  * invalidates this Tile, and frees memory of any member objects
  *  through the deconstructor
  * returns old validity value
@@ -223,6 +129,7 @@ bool Tile::CleanCreateAndValidate(Tile* dst, WorldSegment* segment, df::tiletype
     if (wasValid) {
         dst->~Tile();
     }
+    memset(dst, 0, sizeof(Tile));
     new (dst) Tile(segment, type);
     dst->valid = true;
     return wasValid;

Tile.h
@@ -127,6 +127,8 @@ public:
     bool Invalidate();
     static bool InvalidateAndDestroy(Tile*);
     static bool CleanCreateAndValidate(Tile*, WorldSegment*, df::tiletype);
+
+    friend class WorldSegment;
 };
 
 void createEffectSprites();

WorldSegment.h
@@ -46,7 +46,11 @@ public:
 
         uint32_t newNumTiles = inState.Size.x * inState.Size.y * inState.Size.z;
         uint32_t memoryNeeded = newNumTiles * sizeof(Tile);
-        tiles = new Tile[newNumTiles];
+        tiles = (Tile *)new char[memoryNeeded];
+        //memset(tiles, 0, memoryNeeded);
+        for(uint32_t i = 0; i < newNumTiles; i++) {
+            tiles[i].valid = false;
+        }
     }
 
     ~WorldSegment() {
@@ -56,7 +60,7 @@ public:
         }
         ClearBuildings();
         ClearUnits();
-        delete[] tiles;
+        delete[] (char *)tiles;
     }
 
     void Reset(GameState inState, bool hard=false) {
@@ -72,8 +76,12 @@ public:
         uint32_t memoryNeeded = newNumTiles * sizeof(Tile);
         //if this is a hard reset, or if the size doesn't match what is needed, get a new segment
         if(hard || newNumTiles != getNumTiles()) {
-            delete[] tiles;
-            tiles = new Tile[newNumTiles];
+            delete[] (char *)tiles;
+            tiles = (Tile *)new char[memoryNeeded];
+            for(uint32_t i = 0; i < newNumTiles; i++) {
+                tiles[i].valid = false;
+            }
+            //memset(tiles, 0, memoryNeeded);
         }
 
         segState = inState;