17106d68f19a8944a6dfe74c57f1d4c0d20e777b
Myk Taylor <myk002@yahoo.com>
2023-08-16 23:57:29 -0700

    fix installation path for stonesense resources

--- fix-libs-stonesense.sh
+++ fix-libs-stonesense.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-install_name_tool -change liballegro.5.0.9.dylib @executable_path/hack/libs/liballegro.5.0.dylib stonesense.plug.dylib
-install_name_tool -change liballegro_primitives.5.0.9.dylib @executable_path/hack/libs/liballegro_primitives.5.0.dylib stonesense.plug.dylib
-install_name_tool -change liballegro_image.5.0.9.dylib @executable_path/hack/libs/liballegro_image.5.0.dylib stonesense.plug.dylib
-install_name_tool -change liballegro_font.5.0.9.dylib @executable_path/hack/libs/liballegro_font.5.0.dylib stonesense.plug.dylib
-install_name_tool -change liballegro_ttf.5.0.9.dylib @executable_path/hack/libs/liballegro_ttf.5.0.dylib stonesense.plug.dylib
-install_name_tool -change liballegro_color.5.0.9.dylib @executable_path/hack/libs/liballegro_color.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro.5.0.9.dylib @executable_path/hack/liballegro.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro_primitives.5.0.9.dylib @executable_path/hack/liballegro_primitives.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro_image.5.0.9.dylib @executable_path/hack/liballegro_image.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro_font.5.0.9.dylib @executable_path/hack/liballegro_font.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro_ttf.5.0.9.dylib @executable_path/hack/liballegro_ttf.5.0.dylib stonesense.plug.dylib
+install_name_tool -change liballegro_color.5.0.9.dylib @executable_path/hack/liballegro_color.5.0.dylib stonesense.plug.dylib
 
 echo "Stonesense libs fixed."
