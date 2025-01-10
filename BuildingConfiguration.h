7c5b301ccb14deeb45fea32b6c4229787917478b
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2025-01-10 11:22:32 -0600

    clean `BuldingConfiguration.h`
    convert to header-only class
    rewrite constructors to modern standards

--- BuildingConfiguration.h
+++ BuildingConfiguration.h
@@ -15,7 +15,22 @@ public:
     bool canBeAnySize;
     std::unique_ptr<SpriteNode> sprites;
 
-    BuildingConfiguration(std::string name, int game_type, int game_subtype, int32_t game_custom);
-    BuildingConfiguration();
-    ~BuildingConfiguration(void);
+    BuildingConfiguration(std::string name, int game_type, int game_subtype, int32_t custom) :
+        game_type{ game_type },
+        game_subtype{ game_subtype },
+        game_custom{ custom },
+        width{ 1 },
+        height{ 1 },
+        name{ name },
+        canBeFloating{ false },
+        canBeAnySize{ false },
+        sprites{ nullptr }
+    { }
+    BuildingConfiguration() :
+        BuildingConfiguration{ "", -1, -1, -1 }
+    { }
+    ~BuildingConfiguration()
+    {
+        //cant delete bc.sprites here- screws up BCs copy semantics
+    }
 };
