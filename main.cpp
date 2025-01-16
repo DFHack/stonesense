8b2284ff67c08fc01ed0c01e966f3e434d043de0
Squid Coder <92821989+realSquidCoder@users.noreply.github.com>
2025-01-16 09:33:39 -0600

    Init show_announcement

--- main.cpp
+++ main.cpp
@@ -405,6 +405,7 @@ static void * stonesense_thread(ALLEGRO_THREAD * main_thread, void * parms)
     out.print("Stonesense launched\n");
 
     ssConfig.closeOnEsc = true;
+    ssConfig.show_announcements = true;
     ssConfig.debug_mode = false;
     ssConfig.hide_outer_tiles = false;
     ssConfig.shade_hidden_tiles = true;
