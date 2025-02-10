85b830f4e73949fa1e33e861956deae1ed6c31e7
Squid Coder <92821989+realSquidCoder@users.noreply.github.com>
2025-02-09 19:50:18 -0600

    No immersive mode
    Signed-off-by: Squid Coder <92821989+realSquidCoder@users.noreply.github.com>

--- GameConfiguration.h
+++ GameConfiguration.h
@@ -13,7 +13,6 @@ struct GameConfiguration {
     Config config;
 
     // items not configurable via the config file
-    bool immersive_mode;
     bool show_designations = true;
     bool show_announcements = false;
     bool show_keybinds = false;
